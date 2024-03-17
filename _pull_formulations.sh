867499fdf1f2e48a5770f1c5760c6f3ede23a38c
Fedor Iskhakov <fediskhakov@gmail.com>
2024-03-18 00:11:07 +1100

    Move the util scripts

--- _pull_formulations.sh
+++ _pull_formulations.sh
@@ -3,21 +3,22 @@
 # Argument should be the prefix for the file name to copy, compulsory
 
 # Outside folder with exercises
-source_dir="../../01.tutorial_problems"
+source_dir="../01.tutorial_problems"
+dest_dir="tutorial_problems"
 
 if [ $# -eq 0 ]
   then
     echo "Need prefix for the files to copy"
   else
-    cp $source_dir/$1*.md .
+    cp $source_dir/$1*.md $dest_dir
     # create empty solution file
     for file in $(ls -v $source_dir/$1*.sol.md); do
       fn=$(basename "$file")
       cnt=${fn#*$1}
       cnt=${cnt%.sol*}
-      echo "⏱" > $1$cnt".sol.md"
+      echo "⏱" > $dest_dir/$1$cnt".sol.md"
     done
     # also copy static files
-    cp -r $source_dir/_static ../
+    cp -r $source_dir/_static .
 fi
 
